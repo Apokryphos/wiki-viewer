8a8ea0031b933da89a3839382f863d1ded004820
Tony Slavotinek <apokryphos@users.noreply.github.com>
2017-10-05 07:41:49 -0400

    Removed inline style that set clear button display to none. Fixed issue with clear button hiding when there were search terms but no results. Moved most functions into initialize function.

--- src/index.js
+++ src/index.js
@@ -4,171 +4,173 @@ let thumbnailSize = 128;
 const searchResultCount = 12;
 const searchResultContainers = [];
 
-function hideSearchResultContainer(container) {
-  container.node.style.display = 'none';
-}
+function initialize() {
+  const clearButtonElement = document.getElementById('clear-button');
+  const searchInputElement = document.getElementById('search-input');
 
-function showSearchResultContainer(container) {
-  container.node.style.display = 'block';
-}
-
-function hideSearchResultContainers() {
-  for (let e = 0; e < searchResultCount; ++e) {
-    hideSearchResultContainer(searchResultContainers[e]);
+  function hideSearchResultContainer(container) {
+    container.node.style.display = 'none';
   }
-}
 
-function fetchJson(searchTerm, callback) {
-  const request = new XMLHttpRequest();
-
-  const url = `https://en.wikipedia.org/w/api.php?
-    action=query&
-    origin=*&
-    formatversion=2&
-    generator=prefixsearch&
-    prop=pageimages|pageterms&
-    piprop=thumbnail&
-    pithumbsize=${thumbnailSize}&
-    pilimit=10&
-    gpssearch=${encodeURIComponent(searchTerm)}&
-    gpslimit=${searchResultCount}&
-    redirects=true&
-    wbptterms=description&
-    format=json`;
-
-  request.open('GET', url, true);
-
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      const data = JSON.parse(request.responseText);
-      callback(data);
-    } else {
-      //  Server returned error
-    }
-  };
-
-  request.onerror = function() {};
-
-  request.send();
-}
-
-function updateSearch() {
-  let searchTerm = document.getElementById('search-input').value;
-
-  if (searchTerm) {
-    fetchJson(searchTerm, function(data) {
-      if (!data || !data.query) {
-        document.getElementById('clear-button').style.display = 'none';
-        hideSearchResultContainers();
-      } else {
-        document.getElementById('clear-button').style.display = 'initial';
-        populateSearchResults(data.query.pages);
-      }
-    });
-  } else {
-    document.getElementById('clear-button').style.display = 'none';
-    hideSearchResultContainers();
+  function showSearchResultContainer(container) {
+    container.node.style.display = 'block';
   }
-}
 
-function populateSearchResults(pages) {
-  const pageCount = pages.length;
-  // console.log('populateSearchResults: '' + pageCount );
-
-  for (let e = 0; e < searchResultCount; ++e) {
-    let container = searchResultContainers[e];
-
-    hideSearchResultContainer(container);
-
-    if (e < pageCount) {
-      let page = pages[e];
-
-      container.link.href = 'http://en.wikipedia.org/wiki?curid=' + page.pageid;
-
-      let thumbnail = page.thumbnail;
-      if (thumbnail) {
-        container.img.style.display = 'none';
-        container.img.width = thumbnail.width;
-        container.img.height = thumbnail.height;
-        container.img.src = thumbnail.source;
-        container.img.style.display = 'block';
-      } else {
-        container.img.style.display = 'none';
-        container.img.src = '';
-        container.img.width = thumbnailSize;
-        container.img.height = thumbnailSize;
-      }
-
-      container.title.innerHTML = page.title;
-
-      if (
-        page.terms &&
-        page.terms.description &&
-        page.terms.description.length >= 1
-      ) {
-        container.description.style.display = 'initial';
-        container.description.innerHTML = page.terms.description[0];
-      } else {
-        container.description.style.display = 'none';
-        container.description.innerHTML = '';
-      }
-
-      showSearchResultContainer(container);
-    } else {
-      hideSearchResultContainer(container);
+  function hideSearchResultContainers() {
+    for (let e = 0; e < searchResultCount; ++e) {
+      hideSearchResultContainer(searchResultContainers[e]);
     }
   }
-}
 
-function createSearchResultElements() {
-  //  Find template element for search results
-  let template = document.getElementById('search-result-template');
-  template.id = '';
+  function fetchJson(searchTerm, callback) {
+    const request = new XMLHttpRequest();
 
-  let fragment = document.createDocumentFragment();
+    const url = `https://en.wikipedia.org/w/api.php?
+      action=query&
+      origin=*&
+      formatversion=2&
+      generator=prefixsearch&
+      prop=pageimages|pageterms&
+      piprop=thumbnail&
+      pithumbsize=${thumbnailSize}&
+      pilimit=10&
+      gpssearch=${encodeURIComponent(searchTerm)}&
+      gpslimit=${searchResultCount}&
+      redirects=true&
+      wbptterms=description&
+      format=json`;
 
-  //  Make copies of template and store element references
-  for (let e = 0; e < searchResultCount; ++e) {
-    let node = template.cloneNode(true);
+    request.open('GET', url, true);
 
-    let container = {
-      node: node,
-      link: node,
-      img: node.getElementsByTagName('img')[0],
-      title: node.getElementsByTagName('h2')[0],
-      description: node.getElementsByTagName('p')[0]
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        const data = JSON.parse(request.responseText);
+        callback(data);
+      } else {
+        //  Server returned error
+      }
     };
 
-    searchResultContainers.push(container);
-    fragment.append(node);
+    request.onerror = function() {};
+
+    request.send();
   }
 
-  //  Add template copies to document
-  const searchResultsElement = document.getElementById('search-results');
-  searchResultsElement.append(fragment);
+  function updateSearch() {
+    let searchTerm = searchInputElement.value;
 
-  //  Remove template from document
-  template.remove();
-}
+    if (searchTerm) {
+      fetchJson(searchTerm, function(data) {
+        if (!data || !data.query) {
+          hideSearchResultContainers();
+        } else {
+          populateSearchResults(data.query.pages);
+        }
 
-function clearSearchTerms() {
-  document.getElementById('search-input').value = '';
-  updateSearch();
-}
+        clearButtonElement.style.display = 'initial';
+      });
+    } else {
+      clearButtonElement.style.display = 'none';
+      hideSearchResultContainers();
+    }
+  }
 
-function init() {
+  function populateSearchResults(pages) {
+    const pageCount = pages.length;
+    // console.log('populateSearchResults: '' + pageCount );
+
+    for (let e = 0; e < searchResultCount; ++e) {
+      let container = searchResultContainers[e];
+
+      hideSearchResultContainer(container);
+
+      if (e < pageCount) {
+        let page = pages[e];
+
+        container.link.href = 'http://en.wikipedia.org/wiki?curid=' + page.pageid;
+
+        let thumbnail = page.thumbnail;
+        if (thumbnail) {
+          container.img.style.display = 'none';
+          container.img.width = thumbnail.width;
+          container.img.height = thumbnail.height;
+          container.img.src = thumbnail.source;
+          container.img.style.display = 'block';
+        } else {
+          container.img.style.display = 'none';
+          container.img.src = '';
+          container.img.width = thumbnailSize;
+          container.img.height = thumbnailSize;
+        }
+
+        container.title.innerHTML = page.title;
+
+        if (
+          page.terms &&
+          page.terms.description &&
+          page.terms.description.length >= 1
+        ) {
+          container.description.style.display = 'initial';
+          container.description.innerHTML = page.terms.description[0];
+        } else {
+          container.description.style.display = 'none';
+          container.description.innerHTML = '';
+        }
+
+        showSearchResultContainer(container);
+      } else {
+        hideSearchResultContainer(container);
+      }
+    }
+  }
+
+  function createSearchResultElements() {
+    //  Find template element for search results
+    let template = document.getElementById('search-result-template');
+    template.id = '';
+
+    let fragment = document.createDocumentFragment();
+
+    //  Make copies of template and store element references
+    for (let e = 0; e < searchResultCount; ++e) {
+      let node = template.cloneNode(true);
+
+      let container = {
+        node: node,
+        link: node,
+        img: node.getElementsByTagName('img')[0],
+        title: node.getElementsByTagName('h2')[0],
+        description: node.getElementsByTagName('p')[0]
+      };
+
+      searchResultContainers.push(container);
+      fragment.append(node);
+    }
+
+    //  Add template copies to document
+    const searchResultsElement = document.getElementById('search-results');
+    searchResultsElement.append(fragment);
+
+    //  Remove template from document
+    template.remove();
+  }
+
+  function clearSearchTerms() {
+    searchInputElement.value = '';
+    updateSearch();
+  }
+
   createSearchResultElements();
 
-  const searchInputElement = document.getElementById('search-input');
   searchInputElement.addEventListener('input', function() {
     updateSearch();
   });
 
-  document.getElementById('clear-button').onclick = clearSearchTerms;
+  clearButtonElement.addEventListener('click', clearSearchTerms);
 
   updateSearch();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  init();
+  initialize();
 });
